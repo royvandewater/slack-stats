8d88c305864c8a918f2051e27db7f02d644ecd05
Roy van de Water <roy.vwater@gmail.com>
2018-10-11 18:47:37 -0700

    Get yestday's data

--- slack/slack.go
+++ slack/slack.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"time"
 
-	"github.com/BenLubar/memoize"
 	"github.com/pkg/errors"
 )
 
@@ -16,15 +16,26 @@ type Message struct {
 	User string `json:"user"`
 }
 
-func getMessagesUnmemoized(token, channel string) ([]Message, error) {
+func endTime(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.Local)
+}
+
+func startTime(t time.Time) time.Time {
+	return endTime(t).Add(-24 * time.Hour)
+}
+
+func getMessages(token, channel string) ([]Message, error) {
 	slackURL, err := url.Parse("https://slack.com/api/channels.history")
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to parse URL")
 	}
 
+	now := time.Now()
 	query := slackURL.Query()
 	query.Add("channel", channel)
 	query.Add("count", "1000")
+	query.Add("oldest", fmt.Sprintf("%v.000100", startTime(now).Unix()))
+	query.Add("latest", fmt.Sprintf("%v.000100", endTime(now).Unix()))
 
 	slackURL.RawQuery = query.Encode()
 
@@ -60,8 +71,6 @@ func getMessagesUnmemoized(token, channel string) ([]Message, error) {
 
 	return parsed.Messages, nil
 }
-
-var getMessages = memoize.Memoize(getMessagesUnmemoized).(func(string, string) ([]Message, error))
 
 // GetUserMessages retrieves messages from a particular user in a channel
 func GetUserMessages(token, channel, user string) ([]Message, error) {
